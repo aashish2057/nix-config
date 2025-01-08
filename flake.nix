9e697befb6387d3fe9e1171b94c21395e889d14f
Aashish Sharma <aashish2057@gmail.com>
2025-01-08 09:32:25 -0600

    format flake.nix using alejandra

--- flake.nix
+++ flake.nix
@@ -7,41 +7,36 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    inputs@{
-      self,
-      nix-darwin,
-      nixpkgs,
-    }:
-    let
-      configuration =
-        { pkgs, ... }:
-        {
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
-          environment.systemPackages = [
-            pkgs.neovim
-          ];
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+  }: let
+    configuration = {pkgs, ...}: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [
+        pkgs.neovim
+      ];
 
-          # Necessary for using flakes on this system.
-          nix.settings.experimental-features = "nix-command flakes";
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
 
-          # Set Git commit hash for darwin-version.
-          system.configurationRevision = self.rev or self.dirtyRev or null;
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
 
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          system.stateVersion = 5;
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
 
-          # The platform the configuration will be used on.
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        };
-    in
-    {
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#Aashishs-MacBook-Pro
-      darwinConfigurations."Aashishs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-        modules = [ configuration ];
-      };
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
     };
+  in {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Aashishs-MacBook-Pro
+    darwinConfigurations."Aashishs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [configuration];
+    };
+  };
 }
