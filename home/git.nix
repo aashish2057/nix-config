a08e601763d5f7b905d2af271f4be1a858a6e373
Aashish Sharma <aashish2057@gmail.com>
2025-02-12 07:40:34 -0600

    setup git.nix

---
+++ home/git.nix
@@ -0,0 +1,18 @@
+{lib, ...}: {
+  programs.git = {
+    enable = true;
+
+    userName = "__git config user.name__";
+    userEmail = "__git config user.email__";
+
+    extraConfig = {
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+    };
+
+    delta = {
+      enable = false;
+      options.features = "side-by-side";
+    };
+  };
+}
