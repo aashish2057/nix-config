804189fe409f71366415ed27e08f5d6779a95e93
Aashish Sharma <aashish2057@gmail.com>
2025-02-12 08:04:53 -0600

    cleanup

--- modules/nix-core.nix
+++ modules/nix-core.nix
@@ -6,16 +6,8 @@
   homedir,
   ...
 }: {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = [
-    pkgs.neovim
-  ];
-
-  # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
-  # The platform the configuration will be used on.
   nixpkgs.hostPlatform = system;
 
   # Needed for home manager see https://github.com/nix-community/home-manager/issues/6036
