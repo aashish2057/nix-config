df7dd0687a9f447af0be2a8052f23b03ee190578
Aashish Sharma <aashish2057@gmail.com>
2025-02-12 07:02:16 -0600

    setup core.nix

--- home/default.nix
+++ home/default.nix
@@ -4,25 +4,15 @@
   homedir,
   ...
 }: {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = username;
-  home.homeDirectory = homedir;
-
-  home.packages = with pkgs; [
-    git
+  imports = [
+    ./core.nix
   ];
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "24.11";
+  home = {
+    username = username;
+    homeDirectory = homedir;
+    stateVersion = "24.11";
+  };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
