0772c111c610cd691ce3dd23b7fadf2f7a3c2fc2
Aashish Sharma <aashish2057@gmail.com>
2025-02-21 13:03:23 -0600

    add options.lua

---
+++ dotfiles/nvim/lua/options.lua
@@ -0,0 +1,22 @@
+local opt = vim.opt
+
+-- line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- tabs & indentation
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+-- line wrapping
+opt.linebreak = true
+
+-- search setttings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- apperance
+opt.termguicolors = true
+opt.signcolumn = "auto"
