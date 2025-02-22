375b576f3c658fa82b41e412b9112264e628a698
Aashish Sharma <aashish2057@gmail.com>
2025-02-22 07:02:28 -0600

    setup formatter.lua

---
+++ dotfiles/nvim/lua/config/plugins/formatter.lua
@@ -0,0 +1,47 @@
+return {
+  "jayp0521/mason-null-ls.nvim",
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          "stylua",
+        },
+        automatic_installation = true,
+      })
+      local null_ls = require("null-ls")
+      local formatting = null_ls.builtins.formatting
+      -- local diagnostics = null_ls.builtins.diagnostics
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+      null_ls.setup({
+        sources = {
+          formatting.stylua.with({
+            extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+            filetypes = { "lua" },
+          }),
+        },
+
+        on_attach = function(current_client, bufnr)
+          if current_client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({
+                  filter = function(client)
+                    return client.name == "null-ls"
+                  end,
+                  bufnr = bufnr,
+                })
+              end,
+            })
+          end
+        end,
+      })
+    end,
+  },
+}
