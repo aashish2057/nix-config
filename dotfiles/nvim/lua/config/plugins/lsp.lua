b1c0057f0c98fc869ffad80ff6f62ff4ccf8e1a8
Aashish Sharma <aashish2057@gmail.com>
2025-02-22 06:46:52 -0600

    add mason, mason-lspconfig, remove nodejs_23, lua-language-server

--- dotfiles/nvim/lua/config/plugins/lsp.lua
+++ dotfiles/nvim/lua/config/plugins/lsp.lua
@@ -1,4 +1,6 @@
 return {
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -13,6 +15,13 @@ return {
       },
     },
     config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+        },
+        automatic_installation = true,
+      })
       require("lspconfig").lua_ls.setup({})
     end,
   },
