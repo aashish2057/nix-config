3e9956cdc4fba73825c881e152ce3a0a8b6c2e80
Aashish Sharma <aashish2057@gmail.com>
2025-02-23 07:13:26 -0600

    override devicons, telescope uses mini.icons

--- dotfiles/nvim/lua/config/plugins/mini.lua
+++ dotfiles/nvim/lua/config/plugins/mini.lua
@@ -6,6 +6,13 @@ return {
       require("mini.diff").setup()
       require("mini.git").setup()
       require("mini.icons").setup()
+
+      -- overrides devicons so that telescope uses the icons from mini.icons
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+
       require("mini.statusline").setup()
     end,
   },
