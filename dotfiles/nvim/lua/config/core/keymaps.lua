41411044af8dc74b040baf511e2ca7c566bdf77d
Aashish Sharma <aashish2057@gmail.com>
2025-02-23 07:40:35 -0600

    setup telescope keymaps

--- dotfiles/nvim/lua/config/core/keymaps.lua
+++ dotfiles/nvim/lua/config/core/keymaps.lua
@@ -1,3 +1,18 @@
 -- leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+local keymap = vim.keymap
+
+-- Telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits
+keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer
+keymap.set("n", "<leader>ec", function() -- list files in ~/Desktop/nix-config directory
+  require("telescope.builtin").find_files({
+    cwd = vim.fn.expand("~/Desktop/nix-config"),
+  })
+end)
