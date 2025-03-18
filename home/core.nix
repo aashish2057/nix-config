5001dbf51f25ee12cf320e56e8a221442134bcd9
aashish2057 <aashish2057@gmail.com>
2025-03-18 06:44:49 -0500

    dotfiles: remove tmux

--- home/core.nix
+++ home/core.nix
@@ -16,30 +16,6 @@
       defaultEditor = true;
       vimAlias = true;
     };
-    tmux = {
-      enable = true;
-      baseIndex = 1;
-      prefix = "C-Space";
-      shell = "/bin/zsh";
-      extraConfig = builtins.readFile ../dotfiles/tmux/tmux.conf;
-      plugins = [
-        {
-          plugin = pkgs.tmuxPlugins.resurrect;
-          extraConfig = ''
-            set -g @resurrect-strategy-nvim 'session'
-            set -g @resurrect-capture-pane-contents 'on'
-          '';
-        }
-        {
-          plugin = pkgs.tmuxPlugins.continuum;
-          extraConfig = ''
-            set -g @continuum-restore 'on'
-            set -g @continuum-boot 'on'
-            set -g @continuum-save-interval '10'
-          '';
-        }
-      ];
-    };
   };
 
   home.file = {
