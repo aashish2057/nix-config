4acf9d6990d5ee74ca0db266bbea6cb9ce75b24a
aashish2057 <aashish2057@gmail.com>
2025-03-20 06:29:37 -0500

    nix-test: setup test user

---
+++ users/aashishsharmawork/home.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  config,
+  username,
+  homedir,
+  ...
+}: {
+  imports = [
+    ../../home/core.nix
+    ../../home/git.nix
+    ../../home/shell.nix
+    ../../home/starship.nix
+  ];
+
+  programs.git = {
+    userName = "aashish2059";
+    userEmail = "aashish2057@gmail.com";
+  };
+
+  home.packages = with pkgs; [
+    btop
+  ];
+}
