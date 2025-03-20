af2832758309bac468a2f3085a82b534a9fc307d
aashish2057 <aashish2057@gmail.com>
2025-03-20 18:06:04 -0500

    nix-work: create aashishs-work-macbook-pro host config

---
+++ hosts/aashishs-work-macbook-pro/default.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  pkgs,
+  system,
+  ...
+}: {
+  imports = [
+    ../../modules/nix-core.nix
+    ../../modules/system.nix
+    ../../modules/homebrew.nix
+  ];
+
+  system = {
+    defaults = {
+      dock = {
+        persistent-apps = [
+          "/Applications/Arc.app"
+          "/Applications/Ghostty.app"
+          "/Applications/Slack.app"
+          "/Applications/Zoom.app"
+        ];
+      };
+    };
+  };
+
+  homebrew = {
+    casks = [
+      "slack"
+      "zoom"
+    ];
+  };
+}
