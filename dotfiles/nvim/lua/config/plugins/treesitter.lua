7d4fa41685a2f956256d745f33a87246412b467f
aashish2057 <aashish2057@gmail.com>
2025-03-23 06:44:37 -0500

    dotfiles-nvim: add csharp, typescript to treesitter ensure_installed

--- dotfiles/nvim/lua/config/plugins/treesitter.lua
+++ dotfiles/nvim/lua/config/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "markdown", "nix" },
+        ensure_installed = { "lua", "markdown", "nix", "c_sharp", "typescript" },
         auto_install = false,
         highlight = {
           enable = true,
