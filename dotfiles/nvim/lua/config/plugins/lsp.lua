25bf9f0792e53a81cb02f7408472f4f59e45d422
aashish2057 <aashish2057@gmail.com>
2025-04-10 18:57:03 -0500

    dotfiles-nvim: remove crashdummy registry

--- dotfiles/nvim/lua/config/plugins/lsp.lua
+++ dotfiles/nvim/lua/config/plugins/lsp.lua
@@ -37,7 +37,6 @@ return {
       require("mason").setup({
         registries = {
           "github:mason-org/mason-registry",
-          "github:Crashdummyy/mason-registry",
         },
       })
       require("mason-lspconfig").setup({
