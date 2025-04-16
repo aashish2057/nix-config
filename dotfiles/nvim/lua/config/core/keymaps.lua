70f61a08ac96d63bec0c069ca9dfe78700ac452e
aashish2057 <aashish2057@gmail.com>
2025-04-16 07:04:01 -0500

    dotfiles-nvim: remove telescope

--- dotfiles/nvim/lua/config/core/keymaps.lua
+++ dotfiles/nvim/lua/config/core/keymaps.lua
@@ -9,19 +9,6 @@ keymap.set("n", "<leader>se", "<cmd>lua vim.diagnostic.open_float()<CR>")
 -- Clear trailing whitespace in file
 keymap.set("n", "<Leader>wc", [[:%s/\s\+$//e<cr>:noh<cr>]])
 
--- Telescope
--- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
--- keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
--- keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
--- keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
--- keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits
--- keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer
--- keymap.set("n", "<leader>ec", function() -- list files in ~/Desktop/nix-config directory
---   require("telescope.builtin").find_files({
---     cwd = vim.fn.expand("~/Dev/nix-config"),
---   })
--- end)
-
 -- Snacks
 keymap.set("n", "<leader>ff", function() -- find files
   Snacks.picker.smart()
