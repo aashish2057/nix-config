ecbec86173d23c3dd94b7ad9dfdc92f289262685
aashish2057 <aashish2057@gmail.com>
2025-04-22 15:45:06 -0500

    dotfiles-nvim: replace nonels with conform

--- dotfiles/nvim/lua/config/plugins/formatter.lua
+++ dotfiles/nvim/lua/config/plugins/formatter.lua
@@ -1,54 +1,19 @@
 return {
-  "jayp0521/mason-null-ls.nvim",
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("mason-null-ls").setup({
-        ensure_installed = {
-          "stylua",
-          "csharpier",
-        },
-        automatic_installation = true,
-      })
-      local null_ls = require("null-ls")
-      local formatting = null_ls.builtins.formatting
-      -- local diagnostics = null_ls.builtins.diagnostics
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
-      null_ls.setup({
-        sources = {
-          formatting.stylua.with({
-            extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
-            filetypes = { "lua" },
-          }),
-          formatting.alejandra.with({
-            filetypes = { "nix" },
-          }),
-          formatting.csharpier.with({
-            filetypes = { "cs" },
-          }),
-        },
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			local conform = require("conform")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+			conform.setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- cs = { "csharpier" },
+				},
 
-        on_attach = function(current_client, bufnr)
-          if current_client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({
-                  filter = function(client)
-                    return client.name == "null-ls"
-                  end,
-                  bufnr = bufnr,
-                })
-              end,
-            })
-          end
-        end,
-      })
-    end,
-  },
+				format_on_save = {
+					timeout_ms = 500,
+				},
+			})
+		end,
+	},
 }
