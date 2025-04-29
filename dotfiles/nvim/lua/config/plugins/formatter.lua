9b471c710b568a31ed361e8e64c575d6e7bd08a0
aashish2057 <aashish2057@gmail.com>
2025-04-29 14:21:18 -0500

    dotfiles-nvim: add csharpier to conform autoformat

--- dotfiles/nvim/lua/config/plugins/formatter.lua
+++ dotfiles/nvim/lua/config/plugins/formatter.lua
@@ -3,11 +3,10 @@ return {
 		"stevearc/conform.nvim",
 		config = function()
 			local conform = require("conform")
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
 			conform.setup({
 				formatters_by_ft = {
 					lua = { "stylua" },
-					-- cs = { "csharpier" },
+					cs = { "csharpier" },
 				},
 
 				format_on_save = {
