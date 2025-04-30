203a26940de405b61277e63e529dbee1c3c828de
aashish2057 <aashish2057@gmail.com>
2025-04-30 07:55:54 -0500

    dotfiles-nvim: format all with stylua

--- dotfiles/nvim/lua/config/plugins/snacks.lua
+++ dotfiles/nvim/lua/config/plugins/snacks.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      picker = {
-        matcher = {
-          frecency = true,
-        },
-        debug = {
-          scores = true,
-        },
-      },
-    },
-  },
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				matcher = {
+					frecency = true,
+				},
+				debug = {
+					scores = true,
+				},
+			},
+		},
+	},
 }
