ec0f6d7f84a179679c862af6038a753c917e961a
aashish2057 <aashish2057@gmail.com>
2025-05-03 17:24:50 -0500

    dotfiles-nvim: add c treesitter parser

--- dotfiles/nvim/lua/config/plugins/treesitter.lua
+++ dotfiles/nvim/lua/config/plugins/treesitter.lua
@@ -4,7 +4,17 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "markdown", "nix", "c_sharp", "typescript", "html", "javascript", "yaml" },
+				ensure_installed = {
+					"lua",
+					"markdown",
+					"nix",
+					"c_sharp",
+					"typescript",
+					"html",
+					"javascript",
+					"yaml",
+					"c",
+				},
 				auto_install = false,
 				sync_install = false,
 				ignore_install = {},
