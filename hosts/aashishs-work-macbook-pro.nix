765e451b2d2c9d73eee3f00b8c39d6e8e65c3214
aashish2057 <aashish2057@gmail.com>
2025-06-05 09:30:09 -0500

    nix-work: combine aashishsharamwork.nix and aashishs-work-macbook-pro.nix

--- hosts/aashishs-work-macbook-pro.nix
+++ hosts/aashishs-work-macbook-pro.nix
@@ -40,4 +40,45 @@
       "visual-studio-code"
     ];
   };
+
+  # Home Manager configuration
+  home-manager.users.${username} = {
+    pkgs,
+    config,
+    username,
+    homedir,
+    ...
+  }: {
+    imports = [
+      ../home/core.nix
+      ../home/git.nix
+      ../home/shell.nix
+      ../home/starship.nix
+    ];
+
+    programs.git = {
+      userName = "aashish2057";
+      userEmail = "aashish.sharma@gocrisp.com";
+    };
+
+    home.packages = with pkgs; [
+      docker
+      colima
+      lazydocker
+      # Work project is dotnet 8, roslyn lsp is dotnet 9
+      (with dotnetCorePackages;
+        combinePackages [
+          sdk_8_0
+          sdk_9_0
+        ])
+      dotnet-ef
+      azurite
+      azure-cli
+      pnpm
+      azure-cli
+      kubectl
+      kubelogin
+      k9s
+    ];
+  };
 }
