e4339382459b446cfb42e9afa77aa42f4ddfd677
aashish2057 <aashish2057@gmail.com>
2025-07-07 07:14:34 -0500

    dotfiles-nvim: set auto show completion to false

--- dotfiles/nvim/lua/config/plugins/lsp.lua
+++ dotfiles/nvim/lua/config/plugins/lsp.lua
@@ -63,6 +63,12 @@ return {
 						use_nvim_cmp_as_default = true,
 						nerd_font_variant = "mono",
 					},
+
+					completion = {
+						menu = {
+						  auto_show = false
+						},
+					},
 				},
 			},
 		},
