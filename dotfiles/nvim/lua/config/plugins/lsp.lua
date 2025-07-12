d29111b4273ad362fc1055a407c81c0c20acc666
aashish2057 <aashish2057@gmail.com>
2025-07-12 15:05:09 -0500

    dotfiles-nvim: setup autocompletion keybind

--- dotfiles/nvim/lua/config/plugins/lsp.lua
+++ dotfiles/nvim/lua/config/plugins/lsp.lua
@@ -63,12 +63,6 @@ return {
 						use_nvim_cmp_as_default = true,
 						nerd_font_variant = "mono",
 					},
-
-					completion = {
-						menu = {
-						  auto_show = false
-						},
-					},
 				},
 			},
 		},
