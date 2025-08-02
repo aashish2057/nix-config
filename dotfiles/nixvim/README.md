8ff147751889f878d8aabe91d7174ee09a94efba
aashish2057 <aashish2057@gmail.com>
2025-08-01 19:38:47 -0500

    dotfiles-nvim: init readme

---
+++ dotfiles/nixvim/README.md
@@ -0,0 +1,7 @@
+## Neovim configuration
+
+Reproducable neovim configuration built with [nixCats](https://github.com/BirdeeHub/nixCats-nvim)
+
+#### Startup times
+Barebones: 16.773 ms 
+Current: 19.024 ms
--- dotfiles/nixvim/init.lua
+++ dotfiles/nixvim/init.lua
@@ -1,3 +1,5 @@
+----------OPTIONS----------
+
 -- leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
@@ -7,6 +9,8 @@ local opt = vim.opt
 -- line numbers
 opt.number = true
 opt.relativenumber = true
+
+----------PLUGINS----------
 
 -- colorscheme
 require("material").setup({
