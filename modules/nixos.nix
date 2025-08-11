100e7c059340f9c2abbfa05a5dade4ea5409a1ef
aashish2057 <aashish2057@gmail.com>
2025-08-11 17:41:27 -0500

    nix: more consolidation

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -5,6 +5,10 @@
   hostname,
   ...
 }: {
+  import = [
+    ../hosts/hardware.nix
+  ];
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   nix.settings.experimental-features = ["nix-command" "flakes"];
