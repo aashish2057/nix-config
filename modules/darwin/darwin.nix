4d4cbbd287e1061e19d82eeefc33b0ee21c65a91
aashish2057 <aashish2057@gmail.com>
2025-08-12 17:28:18 -0500

    add darwin/darwin.nix

---
+++ modules/darwin/darwin.nix
@@ -0,0 +1,54 @@
+{
+  self,
+  lib,
+  pkgs,
+  system,
+  isWork,
+  ...
+}: {
+  system = {
+    configurationRevision = self.rev or self.dirtyRev or null;
+    stateVersion = 5;
+    defaults = {
+      dock = {
+        autohide = true;
+        autohide-delay = 0.0;
+        autohide-time-modifier = 0.4;
+        largesize = 97;
+        magnification = true;
+        mru-spaces = false;
+        orientation = "left";
+        show-recents = false;
+        tilesize = 31;
+        persistent-apps =
+          [
+            "/Applications/Helium.app"
+            "/Applications/Ghostty.app"
+          ]
+          ++ lib.optionals isWork [
+            "/Applications/Slack.app"
+            "/Applications/zoom.us.app"
+          ];
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        FXDefaultSearchScope = "SCcf";
+        FXPreferredViewStyle = "Nlsv";
+        NewWindowTarget = "Documents";
+        ShowPathbar = true;
+        ShowRemovableMediaOnDesktop = false;
+        ShowStatusBar = true;
+      };
+      NSGlobalDomain = {
+        InitialKeyRepeat = 15;
+        KeyRepeat = 3;
+        "com.apple.swipescrolldirection" = false;
+        _HIHideMenuBar = true;
+      };
+      WindowManager = {
+        EnableStandardClickToShowDesktop = false;
+      };
+    };
+  };
+}
