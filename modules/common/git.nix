b2949a32b72d8855ad10ff90bb9ade1b369b37c6
aashish2057 <aashish2057@gmail.com>
2025-08-12 16:36:10 -0500

    git nix

--- modules/common/git.nix
+++ modules/common/git.nix
@@ -1,29 +1,33 @@
 {isWork, ...}: {
-  programs.git = {
-    userName = "aashish2057";
-    userEmail =
-      if isWork
-      then "aashish.sharma@gocrisp.com"
-      else "aashish2057@gmail.com";
+  home-manager.sharedModules = [
+    {
+      programs.git = {
+        userName = "aashish2057";
+        userEmail =
+          if isWork
+          then "aashish.sharma@gocrisp.com"
+          else "aashish2057@gmail.com";
 
-    enable = true;
+        enable = true;
 
-    extraConfig = {
-      push.autoSetupRemote = true;
-      pull.rebase = true;
-      fetch.prune = true;
-      fetch.writeCommitGraph = true;
-      branch.sort = "-committerdate";
-      commit.gpgSign = true;
-      tag.gpgSign = true;
-      gpg.format = "ssh";
-      user.signingKey = "~/.ssh/id_ed25519.pub";
-    };
+        extraConfig = {
+          push.autoSetupRemote = true;
+          pull.rebase = true;
+          fetch.prune = true;
+          fetch.writeCommitGraph = true;
+          branch.sort = "-committerdate";
+          commit.gpgSign = true;
+          tag.gpgSign = true;
+          gpg.format = "ssh";
+          user.signingKey = "~/.ssh/id_ed25519.pub";
+        };
 
-    delta = {
-      enable = true;
-      options.features = "side-by-side";
-      options.tabs = 4;
-    };
-  };
+        delta = {
+          enable = true;
+          options.features = "side-by-side";
+          options.tabs = 4;
+        };
+      };
+    }
+  ];
 }
