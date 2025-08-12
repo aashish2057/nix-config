5668e2403549d8b39b09d3c99228713e0e30e83c
aashish2057 <aashish2057@gmail.com>
2025-08-12 15:41:25 -0500

    consolidate git config

--- modules/core.nix
+++ modules/core.nix
@@ -16,7 +16,6 @@
 
   home-manager.users.${username} = {
     imports = [
-      ./git.nix
       ./shell.nix
       ./starship.nix
     ];
@@ -83,12 +82,33 @@
 
     programs = {
       home-manager.enable = true;
+
       git = {
         userName = "aashish2057";
         userEmail =
           if isWork
           then "aashish.sharma@gocrisp.com"
           else "aashish2057@gmail.com";
+
+        enable = true;
+
+        extraConfig = {
+          push.autoSetupRemote = true;
+          pull.rebase = true;
+          fetch.prune = true;
+          fetch.writeCommitGraph = true;
+          branch.sort = "-committerdate";
+          commit.gpgSign = true;
+          tag.gpgSign = true;
+          gpg.format = "ssh";
+          user.signingKey = "~/.ssh/id_ed25519.pub";
+        };
+
+        delta = {
+          enable = true;
+          options.features = "side-by-side";
+          options.tabs = 4;
+        };
       };
     };
   };
