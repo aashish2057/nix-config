77c376e3c09bb38fa2747c0f43f58f06d91b9e4e
aashish2057 <aashish2057@gmail.com>
2025-08-13 13:26:18 -0500

    try neovim

---
+++ dotfiles/neovim/flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    mnw.url = "github:Gerg-L/mnw";
+  };
+  outputs = {
+    nixpkgs,
+    mnw,
+    self,
+    ...
+  }: {
+    packages.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      default = mnw.lib.wrap pkgs {
+        neovim = pkgs.neovim-unwrapped;
+
+        # all files in the `lua/lazy` folder are now autoloaded, so no need
+        # for an init.lua in there
+        initLua = builtins.readFile ./init.lua;
+
+        plugins = {
+          start = [
+            pkgs.vimPlugins.lua-language-server
+            pkgs.vimPlugins.stylua
+            pkgs.vimPlugins.roslyn-ls
+            pkgs.vimPlugins.material-nvim
+            pkgs.vimPlugins.oil-nvim
+            pkgs.vimPlugins.conform-nvim
+            pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+            pkgs.vimPlugins.snacks-nvim
+            pkgs.vimPlugins.no-neck-pain-nvim
+            pkgs.vimPlugins.mini-diff
+            pkgs.vimPlugins.mini-icons
+            pkgs.vimPlugins.mini-statusline
+            pkgs.vimPlugins.vim-fugitive
+            pkgs.vimPlugins.vim-rhubarb
+            pkgs.vimPlugins.roslyn-nvim
+          ];
+
+          # Anything that you're loading lazily should be put here
+          opt = [
+            # pkgs.vimPlugins.telescope-nvim
+          ];
+
+          # dev.myconfig = {
+          #   # you can use lib.fileset to reduce rebuilds here
+          #   # https://noogle.dev/f/lib/fileset/toSource
+          #   pure = ./.;
+          #   impure =
+          #     # This is a hack it should be a absolute path
+          #     # here it'll only work from this directory
+          #     "/' .. vim.uv.cwd()  .. '/nvim";
+          # };
+        };
+      };
+
+      dev = self.packages.x86_64-linux.default.devMode;
+    };
+  };
+}
