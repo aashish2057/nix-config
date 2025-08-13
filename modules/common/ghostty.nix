b7ca26a72ac358162ad913ea4e90a6e8ea2a1992
aashish2057 <aashish2057@gmail.com>
2025-08-13 17:52:48 -0500

    ghostty setup

---
+++ modules/common/ghostty.nix
@@ -0,0 +1,39 @@
+{pkgs, ...}: {
+  home-manager.sharedModules = [
+    {
+      programs.ghostty = {
+        enable = true;
+        enableZshIntegration = true;
+        package = mkif pkgs.stdenv.isDarwin null;
+        clearDefaultKebinds = true;
+        settings =
+          {
+            theme = "MaterialOcean";
+            font-family = "GeistMono NFM";
+            shell-integration-features = "no-cursor";
+            cursor-style = "bar";
+            cursor-style-blink = false;
+            window-theme = "ghostty";
+            keybind = [
+              "ctrl+t=new_tab"
+              "ctrl+w=close_tab"
+              "ctrl+one=goto_tab:1"
+              "ctrl+two=goto_tab:2"
+              "ctrl+three=goto_tab:3"
+              "ctrl+four=goto_tab:4"
+              "ctrl+five=goto_tab:5"
+              "ctrl+six=goto_tab:6"
+              "ctrl+plus=increase_font_size:1"
+              "ctrl+minus=decrease_font_size:1"
+              "ctrl+shift+comma=reload_config"
+            ];
+          }
+          // lib.optionalAttrs pkgs.stdenv.isLinux {
+            gtk-tabs-location = "bottom";
+            gtk-titlebar = false;
+            window-decoration = "none";
+          };
+      };
+    }
+  ];
+}
