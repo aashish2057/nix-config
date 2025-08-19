215fba246f2c518bd72299034814969c5be77ced
aashish2057 <aashish2057@gmail.com>
2025-08-19 14:43:58 -0500

    nix-ghostty: remove custom keybinds

--- modules/common/ghostty.nix
+++ modules/common/ghostty.nix
@@ -20,20 +20,6 @@
             cursor-style = "bar";
             cursor-style-blink = false;
             window-theme = "ghostty";
-            keybind = [
-              "ctrl+t=new_tab"
-              "ctrl+w=close_tab"
-              "ctrl+one=goto_tab:1"
-              "ctrl+two=goto_tab:2"
-              "ctrl+three=goto_tab:3"
-              "ctrl+four=goto_tab:4"
-              "ctrl+five=goto_tab:5"
-              "ctrl+six=goto_tab:6"
-              "ctrl+plus=increase_font_size:1"
-              "ctrl+minus=decrease_font_size:1"
-              "ctrl+shift+comma=reload_config"
-              "super+q=quit"
-            ];
           }
           // lib.optionalAttrs pkgs.stdenv.isLinux {
             gtk-tabs-location = "bottom";
