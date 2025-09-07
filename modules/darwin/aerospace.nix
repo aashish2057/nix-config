4a58a344233b2b51a21ea08ab80c92be00642cf3
aashish2057 <aashish2057@gmail.com>
2025-09-07 12:00:57 -0500

    aerospace: update keybindings

--- modules/darwin/aerospace.nix
+++ modules/darwin/aerospace.nix
@@ -27,37 +27,37 @@
           gaps = {
             inner.horizontal = 5;
             inner.vertical = 5;
-            outer.left = 5;
-            outer.bottom = 5;
-            outer.top = 5;
-            outer.right = 5;
+            outer.left = 0;
+            outer.bottom = 0;
+            outer.top = 0;
+            outer.right = 0;
           };
 
           mode.main.binding = {
-            ctrl-alt-cmd-h = "focus left";
-            ctrl-alt-cmd-j = "focus down";
-            ctrl-alt-cmd-k = "focus up";
-            ctrl-alt-cmd-l = "focus right";
+            alt-h = "focus left";
+            alt-j = "focus down";
+            alt-k = "focus up";
+            alt-l = "focus right";
 
-            ctrl-alt-cmd-shift-h = "move left";
-            ctrl-alt-cmd-shift-j = "move down";
-            ctrl-alt-cmd-shift-k = "move up";
-            ctrl-alt-cmd-shift-l = "move right";
+            alt-shift-h = "move left";
+            alt-shift-j = "move down";
+            alt-shift-k = "move up";
+            alt-shift-l = "move right";
 
-            ctrl-alt-cmd-shift-left = "resize smart +50";
-            ctrl-alt-cmd-shift-right = "resize smart -50";
+            alt-shift-left = "resize smart +50";
+            alt-shift-right = "resize smart -50";
 
-            ctrl-alt-cmd-1 = "workspace 1";
-            ctrl-alt-cmd-2 = "workspace 2";
-            ctrl-alt-cmd-3 = "workspace 3";
+            alt-1 = "workspace 1";
+            alt-2 = "workspace 2";
+            alt-3 = "workspace 3";
 
-            ctrl-alt-cmd-shift-1 = ["move-node-to-workspace 1" "workspace 1"];
-            ctrl-alt-cmd-shift-2 = ["move-node-to-workspace 2" "workspace 2"];
-            ctrl-alt-cmd-shift-3 = ["move-node-to-workspace 3" "workspace 3"];
+            alt-shift-1 = ["move-node-to-workspace 1" "workspace 1"];
+            alt-shift-2 = ["move-node-to-workspace 2" "workspace 2"];
+            alt-shift-3 = ["move-node-to-workspace 3" "workspace 3"];
 
-            ctrl-alt-cmd-t = "layout tiles horizontal vertical";
+            alt-shift-t = "layout tiles horizontal vertical";
 
-            ctrl-alt-cmd-f = ["layout floating tiling" "mode main"];
+            alt-shift-f = ["layout floating tiling" "mode main"];
           };
         };
       };
