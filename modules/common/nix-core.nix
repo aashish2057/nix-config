1a3d5a3ddfd33b99ab0a1f0cbd1bdf6869b24de6
aashish2057 <aashish2057@gmail.com>
2025-11-08 12:48:03 -0600

    nix: add trusted-users, substituers, trusted-public-keys

--- modules/common/nix-core.nix
+++ modules/common/nix-core.nix
@@ -4,7 +4,20 @@
   homedir,
   ...
 }: {
-  nix.settings.experimental-features = "nix-command flakes";
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    trusted-users = ["root" "aashishsharma"];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    ];
+  };
   nixpkgs.hostPlatform = system;
   # Needed for home manager see https://github.com/nix-community/home-manager/issues/6036
   users.users.${username}.home = homedir;
