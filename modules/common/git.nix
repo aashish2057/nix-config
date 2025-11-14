e5dbc16dd2e4ea6fd04851a13190408d44078ede
aashish2057 <aashish2057@gmail.com>
2025-11-14 13:21:29 -0600

    dotfiles-git: update config

--- modules/common/git.nix
+++ modules/common/git.nix
@@ -2,15 +2,16 @@
   home-manager.sharedModules = [
     {
       programs.git = {
-        userName = "aashish2057";
-        userEmail =
-          if isWork
-          then "aashish.sharma@gocrisp.com"
-          else "aashish2057@gmail.com";
-
         enable = true;
 
-        extraConfig = {
+        settings = {
+          user = {
+            name = "aashish2057";
+            email =
+              if isWork
+              then "aashish.sharma@gocrisp.com"
+              else "aashish2057@gmail.com";
+          };
           push.autoSetupRemote = true;
           pull.rebase = true;
           fetch.prune = true;
@@ -20,12 +21,6 @@
           tag.gpgSign = true;
           gpg.format = "ssh";
           user.signingKey = "~/.ssh/id_ed25519.pub";
-        };
-
-        delta = {
-          enable = true;
-          options.features = "side-by-side";
-          options.tabs = 4;
         };
       };
     }
