4467adce3aa4c54ce89f4d18d98f5f00138387b5
aashish2057 <aashish2057@gmail.com>
2025-12-18 09:22:26 -0600

    nix-homelab: enable jellyfin

--- hosts/homeserver/homeserver.nix
+++ hosts/homeserver/homeserver.nix
@@ -9,24 +9,15 @@
     ./hardware.nix
     ../../modules/common/nix-core.nix
     ../../modules/common/home-manager.nix
-    # ../../modules/common/git.nix
     ../../modules/common/shell.nix
     ../../modules/common/starship.nix
     ../../modules/common/neovim/neovim.nix
     ../../modules/common/ghostty.nix
     ../../modules/linux/fonts.nix
-    # ../../modules/linux/hyprland.nix
-    # ../../modules/linux/nvidia.nix
     ../../modules/linux/i18n.nix
-    # ./../modules/linux/xserver.nix
-    # ../../modules/linux/boot.nix
-    # ../../modules/linux/pipewire.nix
-    # ../../modules/linux/hyprland.nix
   ];
 
   system.stateVersion = "25.11";
-
-  # hardware.i2c.enable = true;
 
   networking.hostName = hostname;
   networking.networkmanager.enable = true;
@@ -45,6 +36,10 @@
   services.xserver.xkb = {
     layout = "us";
     variant = "";
+  };
+
+  services.jellyfin = {
+    enable = true;
   };
 
   services.openssh = {
