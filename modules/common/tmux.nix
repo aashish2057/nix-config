b90a152e185fd47e2fdc16c725abbd66e8eaeab2
aashish2057 <aashish2057@gmail.com>
2026-01-23 12:04:20 -0600

    dotfiles-tmux: change colorscheme to kanso

--- modules/common/tmux.nix
+++ modules/common/tmux.nix
@@ -13,8 +13,15 @@
           bind -n S-Left  previous-window
           bind -n S-Right next-window
 
-          set -g status-bg "#0F111A"
+
           set -g status-right "#(date '+%H:%M') | #(date '+%Y-%m-%d')"
+          set -g status-style "bg=#090E13,fg=#c5c9c7"
+
+          set -g window-status-style "bg=#090E13,fg=#5C6066"
+          set -g window-status-current-style "bg=#090E13,fg=#c5c9c7"
+
+          set -g status-left-style "bg=#090E13,fg=#7fb4ca"
+          set -g status-right-style "bg=#090E13,fg=#7fb4ca"
         '';
       };
     }
