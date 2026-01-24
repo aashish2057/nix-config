4588bbeccbed42d029c6aedab816ef79f4b100e6
aashish2057 <aashish2057@gmail.com>
2026-01-24 16:56:56 -0600

    dotfiles-tmux: add tmux sensible, other settings from dmmulroy

--- modules/common/tmux.nix
+++ modules/common/tmux.nix
@@ -7,11 +7,48 @@
         prefix = "C-Space";
         shell = "${pkgs.zsh}/bin/zsh";
         extraConfig = ''
+          # Address vim mode switching delay (http://superuser.com/a/252717/65504)
+          set -s escape-time 0
+
+          # Increase scrollback buffer size from 2000 to 50000 lines
+          set -g history-limit 50000
+
+          # Increase tmux messages display duration from 750ms to 4s
+          set -g display-time 4000
+
+          # Refresh 'status-left' and 'status-right' more often, from every 15s to 5s
+          set -g status-interval 5
+
+          # Upgrade $TERM
+          set -g default-terminal "screen-256color"
+
+          # Enable extended keys (CSI u encoding) for proper modifier key support
+          # Allows Shift+Enter, Ctrl+Shift+<key>, etc. to work correctly in apps
+          set -s extended-keys on
+          set -as terminal-features 'xterm*:extkeys'
+
+          # Emacs key bindings in tmux command prompt (prefix + :) are better than
+          # vi keys, even for vim users
+          set -g status-keys emacs
+
+          # Focus events enabled for terminals that support them
+          set -g focus-events on
+
+          # Super useful when using "grouped sessions" and multi-monitor setup
+          setw -g aggressive-resize on
+
+          # Enable mouse support
+          set -g mouse on
+
           set-option -sa terminal-overrides ",xterm*:Tc"
           set-option -g default-command "${pkgs.zsh}/bin/zsh -l"
 
           bind -n S-Left  previous-window
           bind -n S-Right next-window
+          bind x kill-pane
+
+          # Status bar positioning
+          set-option -g status-position top
 
 
           set -g status-right "#(date '+%H:%M') | #(date '+%Y-%m-%d')"
