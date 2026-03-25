033687eb31779d8710395f23bd6839079207bbfd
aashish2057 <aashish2057@gmail.com>
2026-03-25 10:35:44 -0500

    dotfiles-nvim: run no neck pain only when a buffer is open

--- modules/common/neovim/nvim/lua/config/plugins/neck.lua
+++ modules/common/neovim/nvim/lua/config/plugins/neck.lua
@@ -1,6 +1,18 @@
 require("no-neck-pain").setup({
 	width = 138,
 	autocmds = {
-		enableOnVimEnter = true,
+		disableOnLastBuffer = true,
 	},
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("OpenNoNeckPainOnFirstFile", { clear = true }),
+	callback = function(args)
+		local buftype = vim.bo[args.buf].buftype
+		local name = vim.api.nvim_buf_get_name(args.buf)
+		if buftype == "" and name ~= "" then
+			vim.cmd("NoNeckPain")
+			vim.api.nvim_del_augroup_by_name("OpenNoNeckPainOnFirstFile")
+		end
+	end,
 })
