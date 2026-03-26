49b5567872877dc54f3b048a9535405c63820d5c
aashish2057 <aashish2057@gmail.com>
2026-03-25 20:27:39 -0500

    dotfiles-nvim: remove cs, tf formatters

--- modules/common/neovim/nvim/lua/config/plugins/formatter.lua
+++ modules/common/neovim/nvim/lua/config/plugins/formatter.lua
@@ -2,9 +2,6 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		nix = { "alejandra" },
-		cs = { "csharpier" },
-		tf = { "terraform_fmt" },
-		terraform = { "terraform_fmt" },
 	},
 
 	format_on_save = {
