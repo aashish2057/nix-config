49c003d772267517f79dc6089715a526ca862468
aashish2057 <aashish2057@gmail.com>
2026-04-24 16:07:16 -0500

    nix-nixos: update deprecated settings

--- modules/linux/xserver.nix
+++ modules/linux/xserver.nix
@@ -1,4 +1,4 @@
 {
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 }
