d7f2bc9eacbf9ba268c21adba70e4ab773d35c52
aashish2057 <aashish2057@gmail.com>
2026-07-14 17:44:02 -0500

    dotfiles-quickshell: symlink shell.qml and autostart

--- modules/linux/quickshell.nix
+++ modules/linux/quickshell.nix
@@ -1,13 +1,14 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{...}: {
   home-manager.sharedModules = [
-    {
+    ({config, ...}: {
       programs.quickshell = {
         enable = true;
+        systemd.enable = true;
       };
-    }
+
+      xdg.configFile."quickshell".source =
+        config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/Dev/nix-config/modules/linux/quickshell";
+    })
   ];
 }
