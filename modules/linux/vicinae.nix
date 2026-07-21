4f20075bff199980108793fe286c18d3c7e8fed7
aashish2057 <aashish2057@gmail.com>
2026-07-21 06:17:49 -0500

    dotfiles-vicinae: add suspend and switch input script

--- modules/linux/vicinae.nix
+++ modules/linux/vicinae.nix
@@ -9,6 +9,20 @@
         enable = true;
         systemd.enable = true;
       };
+
+      home.file.".local/share/vicinae/scripts/usb-c-suspend.sh" = {
+        executable = true;
+        text = ''
+          #!${pkgs.runtimeShell}
+          # @vicinae.schemaVersion 1
+          # @vicinae.title Switch to USB-C and Suspend
+          # @vicinae.mode silent
+
+          ${lib.getExe pkgs.ddcutil} setvcp 60 0x0f && \
+            ${pkgs.systemd}/bin/systemd-run --user --quiet --collect \
+              --on-active=5s ${pkgs.systemd}/bin/systemctl suspend
+        '';
+      };
     }
   ];
 }
